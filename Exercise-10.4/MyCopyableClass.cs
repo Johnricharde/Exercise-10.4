49d284573c9e9bbc38c509907257f5361a81ac87
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-12-03 18:20:22 +0100

    Add project files.

---
+++ Exercise-10.4/MyCopyableClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Exercise_10._4
+{
+    class MyCopyableClass
+    {
+        protected int myInt;
+        public int ContainedInt
+        {
+            get
+            { return myInt; }
+            set
+            { myInt = value; }
+        }
+        public MyCopyableClass GetCopy() => (MyCopyableClass)MemberwiseClone();
+    }
+}
---
+++ Exercise-10.4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise_10._4
+{
+    using static System.Console;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj1 = new MyCopyableClass();
+            obj1.ContainedInt = 5;
+            MyCopyableClass obj2 = obj1.GetCopy();
+            obj1.ContainedInt = 9;
+            WriteLine(obj1.ContainedInt);
+            WriteLine(obj2.ContainedInt);
+        }
+    }
+}
